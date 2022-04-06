86acdd62df024648ab4675971d8f51c01b61b94f
max <maxwelgithumbi@gmail.com>
2022-04-06 21:51:21 +0300

    loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const element of array) {
+    newArray.push(appendString + element);
+  }
+
+  return newArray;
+}
+
