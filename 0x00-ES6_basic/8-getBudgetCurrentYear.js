e346e7408d0b18894208ed7ae160a0da210c27b0
max <maxwelgithumbi@gmail.com>
2022-04-06 21:44:13 +0300

    add property

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,10 +4,12 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
+
+  return budget;
 
 }
