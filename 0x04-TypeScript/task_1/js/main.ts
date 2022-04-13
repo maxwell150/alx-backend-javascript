21935150505d524c97b427766933dbfb36a9891c
max <maxwelgithumbi@gmail.com>
2022-04-13 23:12:26 +0300

    students class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,22 +1,48 @@
 interface Teacher {
-    firstName: string;
-    lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [prop: string]: any;
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [prop: string]: any;
 };
 
 
 interface Directors extends Teacher {
-    numberOfReports: number;
+  numberOfReports: number;
 };
 
 interface printTeacherFunction {
-    (firstName: string, lastName: string): string,
+  (firstName: string, lastName: string): string,
 }
   
 export const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
 
 
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working"
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
