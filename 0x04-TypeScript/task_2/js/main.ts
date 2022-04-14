b0e3fa886ef69a744734f987f283aea7e38ea63b
max <maxwelgithumbi@gmail.com>
2022-04-14 18:03:52 +0300

    string literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -26,3 +26,20 @@ export function createEmployee(salary: number | string): Director | Teacher {
     if (typeof salary === 'number' && salary < 500) return new Teacher();
     return new Director();
 }
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+    return employee instanceof Director;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    if (isDirector(employee)) return employee.workDirectorTasks();
+    else return employee.workTeacherTasks();
+}
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') return 'Teaching Math';
+    else if (todayClass === 'History') return 'Teaching History';
+}
+
