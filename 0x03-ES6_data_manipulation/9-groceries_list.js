dbd315041be58b37d1d92e82ba8ed95d812b1432
max <maxwelgithumbi@gmail.com>
2022-04-14 19:47:13 +0300

    map ds

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function groceriesList() {
+  const grocery = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return grocery;
+};
+
