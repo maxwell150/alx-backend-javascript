db850955099fbbadd28d1cf707b973a5c223c9a8
max <maxwelgithumbi@gmail.com>
2022-04-14 19:27:31 +0300

    type arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default function createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position > length - 1) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+
+  return view;
+};
+
