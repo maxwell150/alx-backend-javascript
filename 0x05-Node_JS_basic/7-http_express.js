37038a2126d0afcc50e76c79f3e644136e87c9f2
max <maxwelgithumbi@gmail.com>
2022-05-31 20:27:21 +0300

    http

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DB = args[0];
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const message = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DB);
+    res.end(`${message}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${message}${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Express Server running at http://localhost:${port}/`);
+});
+module.exports = app;
